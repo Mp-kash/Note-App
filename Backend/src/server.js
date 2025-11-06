fe8b2c8b98be24dfaf94e5ce4da87881dea28415
kash munene <158506985+Mp-kash@users.noreply.github.com>
2025-11-06 08:55:26 +0300

    prepared for development

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/noteRoutes.js";
 import { connectDb } from "./config/db.js";
@@ -9,17 +10,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json()); // parse req.body
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Fronted/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../fronted", "dist", "index.html"));
+  });
+}
 
 connectDb().then(() => {
   app.listen(PORT, () => {
